1d517d84a668c6b5861585481f14597dd76e2798
outtaleb <walid.outtaleb@ensta-paris.fr>
2022-12-13 11:50:19 +0100

    Added the pieces' shapes

--- Code/gameElem.hpp
+++ Code/gameElem.hpp
@@ -4,6 +4,15 @@
 #define TERR_NBR_COL 10
 #define PIECE_MAT_SIZE 5
 
+template<class T, size_t n, size_t m>
+void Copy(T source[n][m], T dest[n][m]) {
+    for (int line=0; line<n; line++) {
+        for (int col=0; col<m; col++) {
+            dest[line][col] = source[line][col];
+        }
+    }
+}
+
 enum PieceType { Square, I, L, LMirrored, N, NMirrored, T };
 
 class Piece {
@@ -16,32 +25,89 @@ public:
         switch (type)
         {
         case Square:
-            matrix = {
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0},
+                {0, 0, 1, 1, 0},
                 {0, 0, 0, 0, 0}};
             
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
             break;
+        }
         case I:
-
-            break;
-        case L:
-
-            break;
-        case LMirrored:
-
-            break;
-        case N:
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0}};
             
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
             break;
+        }
+        case L:
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
+            
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
+            break;
+        }
+        case LMirrored:
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0}};
+            
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
+            break;
+        }
+        case N:
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
+            
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
+            break;
+        }
         case NMirrored:
-
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 1, 1, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
+            
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
             break;
+        }
         case T:
-
+        {
+            bool temp[PIECE_MAT_SIZE][PIECE_MAT_SIZE] = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
+            
+            Copy<bool, PIECE_MAT_SIZE, PIECE_MAT_SIZE>(temp, matrix);
             break;
+        }
         default:
             std::cout << "Error - Not a recognized piece type" << std::endl;
             exit(EXIT_FAILURE);

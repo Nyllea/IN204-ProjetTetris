1ff519bc7a1d0a268e0faaf5562b697ca93598de
Paul Poirmeur <ppoirmeur13@gmail.com>
2022-12-13 11:06:19 +0100

    Revert "Created the terrain and piece classes"
    This reverts commit b0cc5c4e498e2536d7284af773064c8dbd0f3ca3.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include "gameElem.hpp"
 
 int main() {
 	std::cout << "Tet" << std::endl;
 
-	Piece piece(N);
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # IN204-ProjetTetris
-
-https://javilop.com/gamedev/tetris-tutorial-in-c-platform-independent-focused-in-game-logic-for-beginners/
